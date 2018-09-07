82b5de4593769764fe83a5a4421cd6cbd2fede84
System Administrator <root@Mac-de-JDLR.local>
2018-09-07 08:24:30 +0200

    maj

---
+++ Promotion/Personne.swift
@@ -0,0 +1,9 @@
+//
+//  Personne.swift
+//  Promotion
+//
+//  Created by JDLR on 05/09/2018.
+//  Copyright © 2018 JDLR. All rights reserved.
+//
+
+import Foundation
---
+++ Promotion/Promotion.swift
@@ -0,0 +1,9 @@
+//
+//  Promotion.swift
+//  Promotion
+//
+//  Created by JDLR on 05/09/2018.
+//  Copyright © 2018 JDLR. All rights reserved.
+//
+
+import Foundation
